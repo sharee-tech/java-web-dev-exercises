e83f1845274734b9a18a3fd9e1fd5387fa00167c
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2021-09-02 21:02:03 -0500

    Studio Area of Circle

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,18 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius:");
+        double radius = input.nextDouble();
+
+//        while (radius <= 0 || !(radius instanceof Double)) {
+//            System.out.println("Must be a positive number: ");
+//            radius = input.nextDouble();
+//        }
+
+        Double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius "+radius+" is: "+area);
+    }
+}
